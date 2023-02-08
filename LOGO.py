371a3f442e9046313e505bb30bd71eb5b850a13a
diamoisara <92915287+diamoisara@users.noreply.github.com>
2023-02-08 13:40:20 +0100

    Create LOGO.py

---
+++ LOGO.py
@@ -0,0 +1,24 @@
+import turtle as t
+
+t.pensize(30)
+t.pencolor("dark orange")
+t.title('Sonatrach logo')
+
+t.forward(140)
+t.backward(70)
+t.left(90)
+t.forward(40)
+t.right(90)
+t.backward(70)
+t.forward(140)
+t.left(90)
+t.forward(40)
+t.right(90)
+t.backward(140)
+t.left(90)
+t.forward(40)
+t.right(90)
+t.forward(140)
+
+turtle.hideturtle()
+t.done ()
